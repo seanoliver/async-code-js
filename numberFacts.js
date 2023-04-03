c34c43416e79edaf2070e18b68b86cd10720e74c
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-03 16:29:48 -0700

    added comments from code review

--- numberFacts.js
+++ numberFacts.js
@@ -4,12 +4,13 @@ const NUMBERS_API_URL = 'http://numbersapi.com';
 
 /**
  * Gets a fact about a single favorite number and prints it on the screen.
+ * TODO: Appends to the dom instead of prints (clearer)
  */
 async function getFavNumFact(num) {
-	const fact = await axios.get(`${NUMBERS_API_URL}/${num}?json`);
+	const response = await axios.get(`${NUMBERS_API_URL}/${num}?json`);
 	const placeOnPage = document.getElementById('fav-num-fact');
 
-	placeOnPage.innerText = fact.data.text;
+	placeOnPage.innerText = response.data.text;
 }
 
 getFavNumFact(42);
@@ -17,11 +18,13 @@ getFavNumFact(42);
 /**
  * Gets a fact about an arbitrary number of numbers and prints each of them on
  * the screen.
+ * TODO: Same comment about "prints"
  */
 async function getMultipleFacts(...nums) {
 	const numString = nums.join(',');
-	const fact = await axios.get(`${NUMBERS_API_URL}/${numString}?json`);
+	const fact = await axios.get(`${NUMBERS_API_URL}/${numString}?json`); // TODO: Change to response
 	const placeOnPage = document.getElementById('fav-num-fact');
+  // TODO: placeOnPage could be clearer (e.g. "displayLocation", etc.)
 
 	for (const num in fact.data) {
 		const newLine = document.createElement('p');
@@ -41,11 +44,12 @@ async function getFourFactsFor(num) {
 	const fact2 = axios.get(`${NUMBERS_API_URL}/${num}?json`);
 	const fact3 = axios.get(`${NUMBERS_API_URL}/${num}?json`);
 	const fact4 = axios.get(`${NUMBERS_API_URL}/${num}?json`);
+  // TODO: Variable names should reflect that these are promises
 
 	const results = await Promise.allSettled([fact1, fact2, fact3, fact4]);
 
 	const placeOnPage = document.getElementById('fav-num-fact');
-	console.log('results', results);
+
 	for (const result of results) {
 		if (result.status === 'fulfilled') {
 			const newLine = document.createElement('p');
