e1671c43f81b3fafac09d798eadd53fe0c191b76
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-03 17:49:58 -0700

    finalized card layering and rotation; added comments from code review

--- deckOfCards.js
+++ deckOfCards.js
@@ -1,55 +1,99 @@
-"use strict";
+'use strict';
 
 let deckId; //will be assigned on page load
+let zCounter; // will be assigned on page load
 
+/**
+ * Controller function to fetch the deck ID and draw a card.
+ */
 // async function start() {
-//   const deckId = await getDeckId();
-//   const card = await drawCard(deckId);
+// 	const deckId = await getDeckId();
+// 	const card = await drawCard(deckId);
 // }
 
+/**
+ * Fetch a new deck ID from the deck of cards API and assign it to the global
+ * variable deckId.
+ */
 async function getDeckId() {
-  const response = await axios.get(
-    'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
-    );
+	const response = await axios.get(
+		'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+	);
 
-  const deckId = response.data.deck_id;
+	const deckId = response.data.deck_id;
 
-  return deckId;
+	return deckId;
 }
 
+/**
+ * Draw two cards from the deck at deckId
+ */
 // async function drawCard(deckId) {
-//   const response1 = await axios.get(
-//     `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-//   );
+// 	const response1 = await axios.get(
+// 		`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+// 	);
 
-//   const response2 = await axios.get(
-//     `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-//   );
+// 	// const response2 = await axios.get(
+// 	//   `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+// 	// );
 
-//   const value1 = response1.data.cards[0].value;
-//   const suit1 = response1.data.cards[0].suit;
+// 	const value1 = response1.data.cards[0].value;
+// 	const suit1 = response1.data.cards[0].suit;
 
-//   const value2 = response2.data.cards[0].value;
-//   const suit2 = response2.data.cards[0].suit;
+//   // const {value, suit} = response1.data.cards[0];
+//   // TODO: destructuring this could make this easier
 
-//   console.log({value1, suit1}, {value2, suit2});
+// 	const value2 = response2.data.cards[0].value;
+// 	const suit2 = response2.data.cards[0].suit;
+
+// 	console.log({ value1, suit1 });
+// 	console.log({value1, suit1}, {value2, suit2});
 // }
 
+/**
+ * Called when page is first loaded, initializes deckId and the zCounter global
+ * variables. TODO: Also empties out card display area.
+ */
 async function start() {
-  deckId = await getDeckId();
+  $('#card-display').empty();
+	deckId = await getDeckId();
+	zCounter = 0;
 }
 
+/**
+ * Draw a new card upon button click and display the card on top of any prior
+ * drawn cards.
+ * TODO: Consider breaking out this function and document capabilities
+ *  -> drawCard
+ *  -> displayCard
+ *  -> controllerFunction that checks if the zCounter is <= 52
+ * TODO: This function doesn't have anything to do with the "button click"
+ * TODO: Refactor the zCounter to instead use the remaining field in the
+ * response from the API (which tracks remaining cards).
+ */
 async function drawCard() {
+  zCounter++;
+
+  if (zCounter > 52) {
+    start();
+    return;
+  }
   const response = await axios.get(
-    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-  );
+		`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+	);
 
-  const cardImgUrl = response.data.cards[0].images.png;
+	const cardImgUrl = response.data.cards[0].images.png;
 
-  const $cardDisplay =  $('#card-display');
-  const $cardImg = $(`<img src=${cardImgUrl}>`);
-  $cardDisplay.append($cardImg);
+	const $cardDisplay = $('#card-display');
+	const $cardImg = $(`<img src=${cardImgUrl}>`);
 
+	$cardImg.attr(
+		'style',
+		`position: absolute;
+    z-index: ${zCounter};
+    transform: rotate(${zCounter}deg);`
+	);
+	$cardDisplay.append($cardImg);
 }
 
 $('#draw-card-btn').on('click', drawCard);
